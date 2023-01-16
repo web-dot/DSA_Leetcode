b371c9e9f025710534df220d593ea2d34eec409f
sudipto <fedev.sudipto@gmail.com>
2023-01-17 01:55:15 +0530

    long[] stack void exception handlers

---
+++ src/com/dsa/stack/StackX.java
@@ -0,0 +1,37 @@
+package com.dsa.stack;
+
+public class StackX {
+	
+	private int maxSize;
+	private long[] stackArray;
+	private int top;
+	
+	
+	public StackX(int s) {
+		maxSize = s;
+		stackArray = new long[maxSize];
+		top = -1;
+	}
+	
+	public void push(long j) {
+		stackArray[++top] = j;
+	}
+	
+	public long pop() {
+		return stackArray[top--];
+	}
+	
+	public long peek() {
+		return stackArray[top];
+	}
+	
+	public boolean isEmpty() {
+		return (top == -1);
+	}
+	
+	public boolean isFull() {
+		return (top == maxSize-1);
+	}
+	
+	
+}
--- src/com/dsa/stack/TestStacks.java
+++ src/com/dsa/stack/TestStacks.java
@@ -4,20 +4,17 @@ public class TestStacks {
 
 	
 	public static void main(String[] args) {
+	
+		StackX theStackX = new StackX(10);
+		theStackX.push(20);
+		theStackX.push(40);
+		theStackX.push(60);
+		theStackX.push(80);
 		
-		ArrayStack s1 = new ArrayStack();
-		s1.push(10);
-		s1.push(20);
-		s1.push(30);
-		//s1.push(40);
+		while(!theStackX.isEmpty()) {
+			long value = theStackX.pop();
+			System.out.println(value);
+		}
 		
-		int x = s1.pop();
-		//System.out.println(x);
-		int y = s1.pop();
-		//System.out.println(y);
-		int z = s1.pop();
-		//System.out.println(z);
-		//int k = s1.pop();
-		System.out.println(s1.top());
 	}
 }
