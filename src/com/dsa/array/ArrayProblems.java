bc3c4f871e3d33a860a7b9d091eae88e2a1de0e0
sudipto <cg.sudipc@gmail.com>
2023-01-26 09:45:42 +0530

    shuffle array-accepted

--- src/com/dsa/array/ArrayProblems.java
+++ src/com/dsa/array/ArrayProblems.java
@@ -68,13 +68,55 @@ public class ArrayProblems {
 	}
 	
 	
+	/*
+	 * PROBLEM 3:
+	 * 
+	 * given the array nums consisting of 2n elements in the form [x1,x2,..,xn,y1,y2,..,yn].
+	 * return the array of the form [x1,y1,x2,y2,..,xn,yn]
+	 * 
+	 * -> can the array be empty ->> supposedly no
+	 * -> can the n be out out of bounds ->> supposedly no
+	 * -> can n be the last index ->> may be
+	 * -> do i have to do this in place ->> supposedly you can, but not mandatory
+	 * example:
+	 * 
+	 * [2,5,1,3,4,7],3 -> [2,3,5,4,1,7]
+	 * [1,2,3,4,4,3,2,1],4 -> [1,4,2,3,3,2,4,1]
+	 * 
+	 * steps:
+	 * so, to approach the problem in a simple way at first:
+	 * 
+	 *  1. i will create a new array of same length
+	 *  2. iterate nums till n and fill up the new array every consecutive element with nums elements
+	 *  3. i will iterate nums from n, and fill up the new array from index 1 and every second index with nums elements.
+	 *  
+	 * 
+	 * */
+	public static int[] shuffle(int[] nums, int n) {
+		int[] base = new int[nums.length];
+		int j = 0;
+		for(int i=0; i<nums.length/2; i++) {
+			base[j] = nums[i];
+			j+=2;
+		}
+		int k = 1;
+		for(int i=n; i<nums.length; i++) {
+				base[k] = nums[i];
+				k+=2;
+		}
+		return base;
+	}
+	
 	public static void main(String[] args) {
 		
+		int[] arr2 = {1,2,3,4,4,3,2,1}; // works
+		int[] arr = {2,5,1,3,4,7}; // works
+ 		System.out.println(Arrays.toString(shuffle(arr2, 4)));
 		
+		/*
 		String[] operations = {"++X","++X","X--"};
 		System.err.println(finalValueAfterOperations(operations));
-		
-		
+		*
 		/*
 		int[] nums = {1, 2, 1};
 		int[] ans = getConcatenation(nums);
