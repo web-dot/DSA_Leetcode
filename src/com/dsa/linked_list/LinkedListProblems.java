3da37983d32d5c7754a70a3c651155aa248abcf2
sudipto <cg.sudipc@gmail.com>
2023-02-05 18:13:37 +0530

    LL160 | find intersection | solving

--- src/com/dsa/linked_list/LinkedListProblems.java
+++ src/com/dsa/linked_list/LinkedListProblems.java
@@ -315,24 +315,60 @@ public class LinkedListProblems {
 		return head;
 	}
 	
+	/**
+	 * P9 : LL 160 : Find the intersection node
+	 * */
+	public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		ListNode p1 = headA;
+		ListNode inter = new ListNode();
+		while(p1.next != null) {
+			p1 = p1.next;
+			ListNode p2 = headB;
+			while(p2.next != null) {
+				p2 = p2.next;
+				if(p1.val == p2.val) {
+					inter = p1;
+				}
+			}
+		}
+		return inter;
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		
-		ListNode l1 = new ListNode(1);
-		ListNode l2 = new ListNode(2);
-		ListNode l3 = new ListNode(3);
+		ListNode l1 = new ListNode(4);
+		ListNode l2 = new ListNode(1);
+		ListNode l3 = new ListNode(8);
 		ListNode l4 = new ListNode(4);
 		ListNode l5 = new ListNode(5);
 //		ListNode l6 = new ListNode(6);
+		
+		ListNode l7 = new ListNode(5);
+		ListNode l8 = new ListNode(6);
+		ListNode l9 = new ListNode(1);
+		ListNode l10 = new ListNode(8);
+		ListNode l11 = new ListNode(4);
+		ListNode l12 = new ListNode(5);
 		
 		l1.next = l2;
 		l2.next = l3;
 		l3.next = l4;
 		l4.next = l5;
-//		l5.next = l6;
 		
-		ListNode n = reverseList(l1);
-
+		l7.next = l8;
+		l8.next = l9;
+		l9.next = l10;
+		l10 = l3;
+		
+		ListNode n = getIntersectionNode(l1, l7);
+		System.out.println(n.val);
+//		ListNode n1 = new ListNode(10);
+//		ListNode n2 = n1;
+//		
+//		System.out.println(n1 == n2);
+		
 	}
 	
 }
