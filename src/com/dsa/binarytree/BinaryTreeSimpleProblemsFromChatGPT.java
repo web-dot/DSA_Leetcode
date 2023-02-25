6a65702422124dd10104e437edbc0f7ff8599764
sudipto <cg.sudipc@gmail.com>
2023-02-25 17:09:29 +0530

    BT P2 | max-depth

--- src/com/dsa/binarytree/BinaryTreeSimpleProblemsFromChatGPT.java
+++ src/com/dsa/binarytree/BinaryTreeSimpleProblemsFromChatGPT.java
@@ -21,6 +21,20 @@ public class BinaryTreeSimpleProblemsFromChatGPT {
 		return 1 + countNodes(root.left) + countNodes(root.right);
 	}
 	
+	/**
+	 * P2:
+	 * Given a binary tree, write a function to find the maximum depth of the tree
+	 * */
+	public static int maxDepth(TreeNode root) {
+		if(root == null) {
+			return 0;
+		}
+		int leftDepth = maxDepth(root.left);
+		int rightDepth = maxDepth(root.right);
+		return Math.max(leftDepth, rightDepth) + 1;
+	}
+	
+	
 	public static void main (String[] args) {
 		TreeNode node = new TreeNode(1);
 		node.left = new TreeNode(2);
@@ -28,6 +42,7 @@ public class BinaryTreeSimpleProblemsFromChatGPT {
 		node.left.left = new TreeNode(4);
 		node.left.right = new TreeNode(5);
 		
-		System.out.println(countNodes(node));
+//		System.out.println(countNodes(node));
+		System.out.println(maxDepth(node));
 	}
 }
