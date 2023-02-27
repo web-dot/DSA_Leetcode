12cdaec20a84c7a7be9ab0010802c69b8556cee9
sudipto <cg.sudipc@gmail.com>
2023-02-27 23:32:31 +0530

    LL203 | given a val, remove all the nodes with same val | solving

--- src/com/dsa/linked_list/LinkedListProblems.java
+++ src/com/dsa/linked_list/LinkedListProblems.java
@@ -376,29 +376,46 @@ public class LinkedListProblems {
 		return true;
 	}
 	
+	
+	/**
+	 * P13: LL203 : Given a LL and an integer val, remove all the node equal to val
+	 * */
+	public static ListNode removeElements(ListNode head, int val) {
+		ListNode current = head;
+		while(current != null && current.next != null) {
+			if(current.val == val) {
+				current.next = current.next.next;
+			}
+			else {
+				current = current.next;
+			}
+		}
+		return head;
+	}
+	
 	public static void main(String[] args) {
 		ListNode l1 = new ListNode(1);
 		ListNode l2 = new ListNode(2);
-		ListNode l3 = new ListNode(1);
-//		ListNode l4 = new ListNode(0);
-		ListNode l5 = new ListNode(1);
-		ListNode l6 = new ListNode(2);
-		ListNode l7 = new ListNode(1);
+		ListNode l3 = new ListNode(6);
+		ListNode l4 = new ListNode(3);
+		ListNode l5 = new ListNode(4);
+		ListNode l6 = new ListNode(5);
+		ListNode l7 = new ListNode(6);
 		
 		l1.next = l2;
 		l2.next = l3;
 		l3.next = l5;
-//		l4.next = l5;
+		l4.next = l5;
 		l5.next = l6;
 		l6.next = l7;
 		
-//		ListNode l = deleteDuplicates(l1);
-		System.out.println(isPalindrome(l1));
+		ListNode l = removeElements(l1, 6);
+//		System.out.println(isPalindrome(l1));
 		
-//		while(l != null) {
-//			System.out.print(l.val + " ");
-//			l = l.next;
-//		}
+		while(l != null) {
+			System.out.print(l.val + " ");
+			l = l.next;
+		}
 	}
 	
 }
