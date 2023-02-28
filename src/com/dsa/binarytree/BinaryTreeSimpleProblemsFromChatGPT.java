1e8752f7b3f17f8343c092c0a0141243d5012e7c
sudipto <cg.sudipc@gmail.com>
2023-02-28 22:33:15 +0530

    BT MAX DEPTH

--- src/com/dsa/binarytree/BinaryTreeSimpleProblemsFromChatGPT.java
+++ src/com/dsa/binarytree/BinaryTreeSimpleProblemsFromChatGPT.java
@@ -60,6 +60,18 @@ public class BinaryTreeSimpleProblemsFromChatGPT {
 		return leftValid && rightValid;
 	}
 	
+	 /**
+    	P2 : MAX DEPTH
+	  */
+	 public static int maxDepthRe(TreeNode root){
+	    if(root == null){
+	       return 0;
+	    }
+	    int leftDepth = maxDepth(root.left);
+	    int rightDepth = maxDepth(root.right);
+	    return Math.max(leftDepth, rightDepth) + 1;
+	 }
+	
 	
 	public static void main (String[] args) {
 		TreeNode node = new TreeNode(5);
