089235a2d8e1ee9de48ad709a12aa076b4ad8086
sudipto <cg.sudipc@gmail.com>
2023-03-03 23:54:29 +0530

    BT DFS

--- src/com/dsa/binarytree/BinaryTreeSimpleProblemsFromChatGPT.java
+++ src/com/dsa/binarytree/BinaryTreeSimpleProblemsFromChatGPT.java
@@ -102,6 +102,25 @@ public class BinaryTreeSimpleProblemsFromChatGPT {
 	      return isMirror(leftSubs.left, rightSubs.right) && isMirror(leftSubs.right, rightSubs.left);
 	  }
 	  
+	  
+	  /**
+      		DFS
+      		
+      		3
+         2     2
+       8  18 15   7 
+      		
+	  */
+   public static void dfs(TreeNode root){
+      if(root == null){
+         return;
+      }
+      System.out.print(root.data + " ");
+      dfs(root.left);
+      dfs(root.right);
+   }
+	  
+	  
 	  /**
 	   * P4 MAX PATH SUM
 	   * */
@@ -112,13 +131,14 @@ public class BinaryTreeSimpleProblemsFromChatGPT {
 	
 	
 	public static void main (String[] args) {
-		 TreeNode root = new TreeNode(1);
+		 TreeNode root = new TreeNode(3);
 	      root.left = new TreeNode(2);
 	      root.right = new TreeNode(2);
-	      root.left.left = new TreeNode(3);
-	      root.left.right = new TreeNode(4);
-	      root.right.left = new TreeNode(4);
-	      root.right.right = new TreeNode(3);
-	      System.out.println(isSymmetric(root, root.left, root.right));
+	      root.left.left = new TreeNode(8);
+	      root.left.right = new TreeNode(18);
+	      root.right.left = new TreeNode(15);
+	      root.right.right = new TreeNode(7);
+//	      System.out.println(isSymmetric(root, root.left, root.right));
+	      dfs(root);
 	}
 }
